6bd50877c815342d27c3a0474dc7454addc9a164
anajikadam <anaji.kadam@paramount.healthcare>
2021-11-25 17:16:23 +0530

    deploy5

--- app/server/routes/ocr.py
+++ app/server/routes/ocr.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 
 from fastapi.responses import HTMLResponse
 
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
+from fastapi.templating import Jinja2Templates
 
 from typing import List
 import os
@@ -18,27 +19,21 @@ from app.server.models.app1 import Drive_OCR
 #     retrieve_students,
 #     update_student,
 # )
-# from app.server.models.ocr import (
-#     ErrorResponseModel,
-#     ResponseModel,
-#     StudentSchema,
-#     UpdateStudentModel,
-# )
+from app.server.models.ocr import (
+    ErrorResponseModel,
+    ResponseModel,
+)
 
 router = APIRouter()
 
 
 @router.get("/")
 async def ocr():
-    # students = await retrieve_students()
-    # if students:
-    #     return ResponseModel(students, "Students data retrieved successfully")
-    # return ResponseModel(students, "Empty list returned")
-    return {"Hello": "World"}
+    return {"Data": "Hello World"}
 
-@router.post("/get_file/")
-async def get_file(file: UploadFile = File(...)):
-    print(file.file) # temp file save
+@router.post("/getText/")
+async def get_Text(file: UploadFile = File(...)):
+    # print(file.file) # temp file save
     f = file.filename.replace(" ", "-")
     t = time.localtime()
     timestamp = time.strftime('%b-%d-%Y_%H%M', t)
@@ -50,108 +45,51 @@ async def get_file(file: UploadFile = File(...)):
         f.close()
     ob = Drive_OCR(file_name)
     text = ob.main()
-    os.remove(file_name)
     text = text[17:]
-    return {"info": file.filename,
-            "Text":text}
+    os.remove(file_name)
+    if len(text)<1:
+        return ErrorResponseModel("An error occurred",
+                404,
+                "No text found in given Image", )
+    return ResponseModel(text, "OCR Extract Data successfully.")
 
 
-@router.get("/get_image/")
-async def get_image():
-    content = """
-    <body>
-   <form action="/ocr/get_file/" enctype="multipart/form-data" method="post">
-    <input name="file" type="file">
-    <input type="submit">
-    </form>
-    </body>
-        """
-    return HTMLResponse(content=content)
+@router.post("/get_file/")
+async def get_file(file: UploadFile = File(...)):
+    # print(file.file) # temp file save
+    f = file.filename.replace(" ", "-")
+    t = time.localtime()
+    timestamp = time.strftime('%b-%d-%Y_%H%M', t)
+    fileName = timestamp + '_' + f
+    file_name = fileName
+    # file_name = "app/server/static/images/"+fileName
+    with open(file_name,'wb+') as f:
+        f.write(file.file.read())
+        f.close()
+    ob = Drive_OCR(file_name)
+    text = ob.main()
+    text = text[17:]
+    os.remove(file_name)
+    if len(text)<1:
+        return ErrorResponseModel("An error occurred",
+                404,
+                "No text found in given Image", )
+    d = {"File Name": file_name, "Extracted Text":text}
+    return ResponseModel(d, "OCR Extract Data successfully.")
 
 
-
-# @router.post("/files1/")
-# async def create_files(files: List[bytes] = File(...)):
-#     return {"file_sizes": [len(file) for file in files]}
-
-
-# @router.post("/files/")
-
-# async def create_files(files: List[bytes] = File(...)):
-
-#     return {"file_sizes": [len(file) for file in files]}
+@router.get("/get_image/",response_class=HTMLResponse )
+async def get_image(request: Request, ):
+    templates = Jinja2Templates(directory="app/server/templates")
+    return templates.TemplateResponse("index1.html", {"request": request,})
 
 
-# @router.post("/uploadfiles/")
-
-# async def create_upload_files(files: List[UploadFile] = File(...)):
-
-#     return {"filenames": [file.filename for file in files]}
-
-
-# @router.get("/aa/")
-# async def main():
 #     content = """
 #     <body>
-#     <form action="/files/" enctype="multipart/form-data" method="post">
-#     <input name="files" type="file" multiple>
-#     <input type="submit">
-#     </form>
-#     <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-#     <input name="files" type="file" multiple>
+#    <form action="/ocr/get_file/" enctype="multipart/form-data" method="post">
+#     <input name="file" type="file">
 #     <input type="submit">
 #     </form>
 #     </body>
-#     """
+#         """
 #     return HTMLResponse(content=content)
-
-
-# @router.post("/", response_description="Student data added into the database")
-# async def add_student_data(student: StudentSchema = Body(...)):
-#     student = jsonable_encoder(student)
-#     new_student = await add_student(student)
-#     return ResponseModel(new_student, "Student added successfully.")
-
-
-# @router.get("/", response_description="Students retrieved")
-# async def get_students():
-#     students = await retrieve_students()
-#     if students:
-#         return ResponseModel(students, "Students data retrieved successfully")
-#     return ResponseModel(students, "Empty list returned")
-
-
-# @router.get("/{id}", response_description="Student data retrieved")
-# async def get_student_data(id):
-#     student = await retrieve_student(id)
-#     if student:
-#         return ResponseModel(student, "Student data retrieved successfully")
-#     return ErrorResponseModel("An error occurred.", 404, "Student doesn't exist.")
-
-
-# @router.put("/{id}")
-# async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
-#     req = {k: v for k, v in req.dict().items() if v is not None}
-#     updated_student = await update_student(id, req)
-#     if updated_student:
-#         return ResponseModel(
-#             "Student with ID: {} name update is successful".format(id),
-#             "Student name updated successfully",
-#         )
-#     return ErrorResponseModel(
-#         "An error occurred",
-#         404,
-#         "There was an error updating the student data.",
-#     )
-
-
-# @router.delete("/{id}", response_description="Student data deleted from the database")
-# async def delete_student_data(id: str):
-#     deleted_student = await delete_student(id)
-#     if deleted_student:
-#         return ResponseModel(
-#             "Student with ID: {} removed".format(id), "Student deleted successfully"
-#         )
-#     return ErrorResponseModel(
-#         "An error occurred", 404, "Student with id {0} doesn't exist".format(id)
-#     )
